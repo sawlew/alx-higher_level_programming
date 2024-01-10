2d0b06f0cba56b7f4f91f00f086613e3905a3dfc
Soliu Adeosun <sawlew@gmail.com>
2024-01-10 11:43:07 +0100

    okay

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of a 200 status code response
+# Get the response body for a given URL for 200 status code responses.
 curl -sL "$1"
